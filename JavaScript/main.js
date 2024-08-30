bc9893f1ebaa7aa9b1ab6e6caa18a43899eea8b3
Facundo <marcofacundolucas@gmail.com>
2024-08-30 16:11:27 -0300

    update

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,12 +1,11 @@
-        //Cuando el usuario haga scroll en la pagina, se ejecuta la funcion barraProgreso
+//Cuando el usuario haga scroll en la pagina, se ejecuta la funcion barraProgreso
 window.onscroll = function(){barraProgreso()};
 
 function barraProgreso() {
-        let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
 
-        let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-        let scrolled = (winScroll / height) * 100;
-        
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
     document.getElementById("miBarra").style.width = scrolled + "%";
 }
 
@@ -15,10 +14,8 @@ function barraProgreso() {
 
 
 
-
-
-        //Cuando el usuario salga del Sitio Web se cambia el TITULO
-    let tituloAnterior = document.title
+//Cuando el usuario salga del Sitio Web se cambia el TITULO
+let tituloAnterior = document.title
 
 window.addEventListener("blur", () => {
     tituloAnterior = document.title
